9f2f48ffe301c5ad98fea7ed67211b4baf3db67c
PhuocSE172185 <105037635+PhuocSE172185@users.noreply.github.com>
2025-07-25 08:17:25 +0700

    Update CopyRight.jsx

--- src/Components/CopyRight/CopyRight.jsx
+++ src/Components/CopyRight/CopyRight.jsx
@@ -5,7 +5,7 @@ export default function CopyRight() {
   return (
     <div className='footer-copyright'>
       <div className="container copyright-container">
-        <p>© 2024 Zapterify. All rights reserved.</p>
+        <p>© 2024 Booklify. All rights reserved.</p>
         <div className="footer-socials">
           {FootersLinksData.socials.map((item, index) => (
             <a href={item.link} key={index}>  
